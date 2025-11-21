a766e420b504cb80399cc2d8200d9d1c9216fa82
Farhan <ullah8113@gmail.com>
2025-11-21 23:16:22 +0600

    Problem Service by Java

---
+++ problemservice/src/main/java/com/codearena/problemservice/ProblemserviceApplication.java
@@ -0,0 +1,12 @@
+package com.codearena.problemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProblemserviceApplication.class, args);
+    }
+}
