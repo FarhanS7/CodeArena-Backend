e694ef0af726d4c4449f73979b2c3bb0a54386b8
Farhan <ullah8113@gmail.com>
2026-05-07 00:17:48 +0600

    config: use DATABASE_URL in execution-service

--- execution-service/src/config/ormconfig.ts
+++ execution-service/src/config/ormconfig.ts
@@ -2,11 +2,12 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const ormConfig = (): TypeOrmModuleOptions => ({
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '5432', 10),
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
-  database: process.env.DB_DATABASE || 'execution_db',
+  url: process.env.DATABASE_URL,
+  host: !process.env.DATABASE_URL ? process.env.DB_HOST || 'localhost' : undefined,
+  port: !process.env.DATABASE_URL ? parseInt(process.env.DB_PORT || '5432', 10) : undefined,
+  username: !process.env.DATABASE_URL ? process.env.DB_USERNAME || 'postgres' : undefined,
+  password: !process.env.DATABASE_URL ? process.env.DB_PASSWORD || 'postgres' : undefined,
+  database: !process.env.DATABASE_URL ? process.env.DB_DATABASE || 'execution_db' : undefined,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: process.env.NODE_ENV !== 'production', // Auto-create tables in development
   logging: process.env.NODE_ENV !== 'production',
